bba84dcded188024b73f7dea01604e8ca64a07d5
Blake Tucker <blaketucker12@yahoo.com>
2021-12-05 21:19:36 -0600

    codequiz js

--- game.js
+++ game.js
@@ -1,1 +1,121 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull')
 
+let currentQuestion= {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'Which of these is a programming language?',
+        choice1: 'Bite',
+        choice2: 'Scratch',
+        choice3: 'Gnaw',
+        choice4: 'Itch',
+        answer: 2,
+    },
+    {
+        question: 'Which of these does not run on a computer program?',
+        choice1: 'Rocket',
+        choice2: 'Bike',
+        choice3: 'Train',
+        choice4: 'Car',
+        answer: 2,
+    },
+    {
+        question: 'What is the most common programming language?',
+        choice1: 'Javascript',
+        choice2: 'Python',
+        choice3: 'Java',
+        choice4: 'C++',
+        answer: 1,
+    },
+    {
+        question: 'More than one instruction or command followed in order?',
+        choice1: 'Bug',
+        choice2: 'Algorithm',
+        choice3: 'Command',
+        choice4: 'Sequence',
+        answer: 4,
+    },
+    {
+        question: 'How many programming languages are there?',
+        choice1: 'Around 10',
+        choice2: 'Around 25',
+        choice3: 'Around 700',
+        choice4: 'Around 40,000',
+        answer: 3,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion  = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.asssign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = 'Questions ${questionCounter} of ${MAX_QUESTIONS}'
+    progressBarFull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
